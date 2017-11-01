a719ac37f2856ed85ed4900ee01dd16d91a100fd
lukney <sangeetha18lukney@gmail.com>
2017-11-01 23:26:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //mysql 
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host : 'localhost',
-    user : 'root',
-    password : '',
-    database : 'mydatabase',
+    host : 'nemai1234.cyelmj7smxyj.us-east-1.rds.amazonaws.com',
+    user : 'nemai1234',
+    password : 'nemai1234',
+    database : 'nemai1234',
 	port :'3306'
     
     
@@ -58,7 +58,7 @@ app.post('/insert', function(req,res){
  
 var server = app.listen(3009, function () {
  
-  var host = server.address().address
+  var host = 'ec2-54-196-233-252.compute-1.amazonaws.com'
   var port = server.address().port
  
   console.log("Example app listening at http://%s:%s", host, port)
